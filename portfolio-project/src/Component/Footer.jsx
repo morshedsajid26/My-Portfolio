b21a13c5833d9ecb634f19f89434cb8e40184151
Neaz Morshed Sajid <160872389+morshedsajid26@users.noreply.github.com>
2024-11-21 23:59:22 +0600

    .

--- portfolio-project/src/Component/Footer.jsx
+++ portfolio-project/src/Component/Footer.jsx
@@ -1,18 +1,28 @@
-import React from 'react'
-import Container from './Layer/Container'
+import React from 'react';
+import Container from './Layer/Container';
 
 const Footer = () => {
+  // Get the current year dynamically
+  const currentYear = new Date().getFullYear();
+
   return (
-    <div className='bg-[#212529] text-center py-[66px]'>
+    <div className="bg-[#212529] text-center py-6 md:py-10">
       <Container>
         <div>
-          <p className='font-pop text-[#FAFAFA]/80 '> Copyright © 2022 
-            <a href="" className='text-[#20c997]'> Sajid</a> 
-          . All Rights Reserved. </p>
+          <p className="font-pop text-sm md:text-base text-[#FAFAFA]/80 px-4">
+            Copyright © {currentYear}{' '}
+            <a 
+              href="#" 
+              className="text-[#20c997] hover:text-[#17a586] transition-all duration-300"
+            >
+              Sajid
+            </a>
+            . All Rights Reserved.
+          </p>
         </div>
       </Container>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
