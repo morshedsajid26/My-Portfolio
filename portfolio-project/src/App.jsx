1915673315a42c340961a2e94e0bf958af7adad2
Neaz Morshed Sajid <160872389+morshedsajid26@users.noreply.github.com>
2024-12-08 19:10:10 +0600

    whatsapp link done

--- portfolio-project/src/App.jsx
+++ portfolio-project/src/App.jsx
@@ -8,9 +8,11 @@ import Client from './Component/Pages/Client'
 import Contact from './Component/Pages/Contact'
 import Footer from './Component/Footer'
 
+import whatsapp from '/whatsapp.png'
+
 const App = () => {
   return (
-    <div>
+    <div className='relative'>
       <Navbar/>
         <About/>
         <Services/>
@@ -19,6 +21,13 @@ const App = () => {
         <Client/>
         <Contact/>
         <Footer/>
+        <div className='fixed bottom-5 right-5 h-12 w-12  animate-bounce   '>
+        <a href="https://wa.me/+8801756899699" target="_blank">
+  <img  src={whatsapp} alt="WhatsApp" />
+</a>
+
+   
+        </div>
     </div>
   )
 }
