72869ca39ef1adfee1bdc504da285af4ab15d7e7
Neaz Morshed Sajid <160872389+morshedsajid26@users.noreply.github.com>
2024-12-10 16:23:03 +0600

    TExtAnimatin responsive done

--- portfolio-project/src/Component/Layer/TextAnimation.jsx
+++ portfolio-project/src/Component/Layer/TextAnimation.jsx
@@ -4,7 +4,7 @@ import { Typewriter } from "react-simple-typewriter";
 const TextAnimation = () => {
   return (
     <div className="flex justify-center items-center">
-      <h1 className="text-[64px] font-semibold text-[#ffffff] font-pop">
+      <h1 className="text-[32px] sm:text-[48px] md:text-[64px] lg:text-[72px] font-semibold text-[#ffffff] font-pop">
         I'm 
         <Typewriter
           words={[" Syed Neaz Morshed Sajid", " Frontend Developer.", " Freelancer."]}
