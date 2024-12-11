3a29926b04422a78256f03eb12effd7e079f15a7
Neaz Morshed Sajid <160872389+morshedsajid26@users.noreply.github.com>
2024-12-11 22:51:31 +0600

    .

---
+++ portfolio-project/src/Component/Layer/ProjectCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ProjectCard = () => {
+  return (
+    <div className='w-[400px] h-[300px] bg-slate-300'>
+        
+        
+    </div>
+  )
+}
+
+export default ProjectCard
--- portfolio-project/src/Component/Pages/Portfolio.jsx
+++ portfolio-project/src/Component/Pages/Portfolio.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Container from '../Layer/Container'
 import TitleHeader from '../Layer/TitleHeader'
+import ProjectCard from '../Layer/ProjectCard'
 
 const Portfolio = () => {
   return (
     <div id='portfolio' className='bg-[#343a40] py-[72px]'>
          <Container>
         <TitleHeader Heading='portfolio' Ptext='my work'/>
+
+        <ProjectCard/>
         </Container>
     </div>
   )
