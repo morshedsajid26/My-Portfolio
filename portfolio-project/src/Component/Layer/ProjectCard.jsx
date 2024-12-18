9f1553221569987feef2f2fe79868db120d8a003
Neaz Morshed Sajid <160872389+morshedsajid26@users.noreply.github.com>
2024-12-18 23:47:09 +0600

    running

--- portfolio-project/src/Component/Layer/ProjectCard.jsx
+++ portfolio-project/src/Component/Layer/ProjectCard.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import orebi from '/Orebi.png'
+import { FiExternalLink } from 'react-icons/fi'
 
 
 
@@ -12,14 +13,36 @@ const ProjectCard = () => {
 
         <div className=' absolute w-[500px]  h-[300px] top-0 cursor-pointer bg-black/50 flex flex-col justify-between'>
 
+        
+
         <h3 className='text-[47px]'>Orebi</h3>
 
-        <div className="tools flex gap-3">
+       <div className=' flex justify-between'>
+       <div className="tools flex gap-3">
           <h5 className='px-4 py-2 bg-slate-500 rounded-full font-semibold text-[16px]'>HTML5</h5>
           <h5  className='px-4 py-2 bg-slate-500 rounded-full font-semibold text-[16px]'>Tailwind</h5>
           <h5  className='px-4 py-2 bg-slate-500 rounded-full font-semibold text-[16px]'>JavaScript</h5>
           <h5  className='px-4 py-2 bg-slate-500 rounded-full font-semibold text-[16px]'>React</h5>
+
+
+
+
+
+
+
+
+          
         </div>
+
+
+        <div className='px-3 py-2 bg-slate-500 rounded-full text-black w-10 flex justify-center items-center visibility-full'  >
+        <a href="">
+        <FiExternalLink  />
+        </a>
+        </div>
+       </div>
+
+        
 
 
         </div>
