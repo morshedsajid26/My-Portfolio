b526d90ae96f3a6da4045be72e6da4132c8cc27f
Neaz Morshed Sajid <160872389+morshedsajid26@users.noreply.github.com>
2024-12-18 22:55:25 +0600

    projectcard running

--- portfolio-project/src/Component/Layer/ProjectCard.jsx
+++ portfolio-project/src/Component/Layer/ProjectCard.jsx
@@ -6,11 +6,20 @@ import orebi from '/Orebi.png'
 
 const ProjectCard = () => {
   return (
-    <div className={`relative after:bg-black/50 after:absolute after:content-[""] after:w-[500px] after:h-[300px] after:top-0 after:cursor-pointer after:invisible group-hover/item:visible `}>
+    <div className={`relative after: after: after:content-[""] after: after: after: after: after:invisible group-hover/item:visible font-pop `}>
       
         <img className='w-[500px] h-[300px] ' src={orebi}  alt="" />
 
-        <div className=''>
+        <div className=' absolute w-[500px]  h-[300px] top-0 cursor-pointer bg-black/50 flex flex-col justify-between'>
+
+        <h3 className='text-[47px]'>Orebi</h3>
+
+        <div className="tools flex gap-3">
+          <h5 className='px-4 py-2 bg-slate-500 rounded-full font-semibold text-[16px]'>HTML5</h5>
+          <h5  className='px-4 py-2 bg-slate-500 rounded-full font-semibold text-[16px]'>Tailwind</h5>
+          <h5  className='px-4 py-2 bg-slate-500 rounded-full font-semibold text-[16px]'>JavaScript</h5>
+          <h5  className='px-4 py-2 bg-slate-500 rounded-full font-semibold text-[16px]'>React</h5>
+        </div>
 
 
         </div>
