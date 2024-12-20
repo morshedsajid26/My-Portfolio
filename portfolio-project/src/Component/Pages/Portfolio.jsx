b1fe1b0f7ac68ccdf55c4c7038c46306922c154a
Neaz Morshed Sajid <160872389+morshedsajid26@users.noreply.github.com>
2024-12-20 23:47:44 +0600

    .

--- portfolio-project/src/Component/Pages/Portfolio.jsx
+++ portfolio-project/src/Component/Pages/Portfolio.jsx
@@ -120,47 +120,7 @@ const Portfolio = () => {
            </div>
 
 
-           <div className={` relative  font-pop`}>
-             
-               <img className='w-screen max-w-[624px]  h-[300px] rounded-lg  ' src={orebi}  alt="" />
-       
-               <div className=' absolute w-screen max-w-[624px]   h-[300px] top-0  bg-black/65 flex flex-col rounded-lg opacity-0 hover:opacity-100 transition-all duration-500  '>
-       
-               
-       
-               <h3 className='text-[30px] md:text-[40px] text-white/70 text-center'>OREBI</h3>
-       
-               <div className='mx-auto my-5'>
-       
-               <h3 className='text-[18px] md:text-[22px] text-white/70 text-center'>Technologies</h3>
-       
-       
-               <div className="tools flex gap-2 mt-5  ">
-                 <h5 className=' px-[10px] md:px-4 py-1  md:py-2 bg-[#212529]/80 text-white/70 rounded-full font-semibold  text-[12px] md:text-[16px]'>HTML5</h5>
-                 <h5  className=' px-[10px] md:px-4 py-1  md:py-2 bg-[#212529]/80 text-white/70 rounded-full font-semibold text-[12px] md:text-[16px]'>Tailwind CSS</h5>
-                 <h5  className=' px-[10px] md:px-4 py-1  md:py-2 bg-[#212529]/80 text-white/70 rounded-full font-semibold text-[12px] md:text-[16px]'>JavaScript</h5>
-                 <h5  className=' px-[10px] md:px-4 py-1  md:py-2 bg-[#212529]/80 text-white/70 rounded-full font-semibold text-[12px] md:text-[16px]'>React</h5>       
-               </div>
-       
-               </div>
-       
-              <div className=' flex justify-between'>
-              
-       
-       
-               <div className=' px-[10px] md:px-3 py-1 md:py-2 bg-[#212529]/80 text-white/70 rounded-full  flex justify-center items-center cursor-pointer mx-auto font-medium  mt-8'  >
-               <a className='flex justify-center items-center gap-2 text-[12px] md:text-[16px]' href=""> <p>Live Link</p>
-               <FiExternalLink  />
-               </a>
-               </div>
-              </div>
-       
-               
-       
-       
-               </div>
-               
-           </div>
+           
 
 
            <div className={` relative  font-pop`}>
