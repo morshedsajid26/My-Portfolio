e93afe50572ae1d83e85ea32f75043c3e248ddb7
Neaz Morshed Sajid <160872389+morshedsajid26@users.noreply.github.com>
2024-12-26 10:44:08 +0600

    textAnimation updated

--- portfolio-project/src/Component/Layer/TextAnimation.jsx
+++ portfolio-project/src/Component/Layer/TextAnimation.jsx
@@ -5,9 +5,9 @@ const TextAnimation = () => {
   return (
     <div className="flex justify-center items-center  h-[100px]">
       <h1 className="text-[32px] sm:text-[48px] md:text-[64px] lg:text-[72px] font-semibold text-[#ffffff] font-pop">
-        I'm 
+        I'm
         <Typewriter
-          words={[" Syed Neaz Morshed Sajid", " Frontend Developer.", " Freelancer."]}
+          words={[" Syed Neaz Morshed Sajid", " a Frontend Developer.", " a Freelancer."]}
           loop={Infinity}
           cursor
           cursorStyle="|"
