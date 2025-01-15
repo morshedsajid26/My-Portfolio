d60df0697b80df10cfdd07802924ea2fb29b60d3
Neaz Morshed Sajid <160872389+morshedsajid26@users.noreply.github.com>
2025-01-15 20:22:36 +0600

    text animation updated

--- portfolio-project/src/Layer/TextAnimation.jsx
+++ portfolio-project/src/Layer/TextAnimation.jsx
@@ -7,7 +7,7 @@ const TextAnimation = () => {
       <h1 className="text-[32px] sm:text-[48px] md:text-[64px] lg:text-[72px] font-semibold text-[#ffffff] font-pop">
         I'm
         <Typewriter
-          words={[" Syed Neaz Morshed Sajid", " a Frontend Developer.", " a Freelancer."]}
+          words={[" Syed Neaz Morshed Sajid", " a Frontend Developer."]}
           loop={Infinity}
           cursor
           cursorStyle="|"
